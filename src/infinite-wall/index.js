f282cfd5550b06f42bdf1fee4430f4e077296d1c
Derek Nguyen <derek@penandpillow.com>
2018-11-30 14:25:24 +0900

    snap progress

--- src/infinite-wall/index.js
+++ src/infinite-wall/index.js
@@ -1,4 +1,4 @@
-import Hammer from 'hammerjs';
+import Hammer from "hammerjs";
 
 export default class {
   constructor($wall) {
@@ -10,29 +10,39 @@ export default class {
     this.data = Array.from(Array(dataLength).keys());
 
     const hammer = new Hammer.Manager($wall);
-    hammer.add(new Hammer.Pan({
-      // pointer: 2,
-      direction: Hammer.DIRECTION_ALL,
-      threshold: 0 
-    }));
+    hammer.add(
+      new Hammer.Pan({
+        // pointer: 2,
+        direction: Hammer.DIRECTION_ALL,
+        threshold: 0
+      })
+    );
 
     this.wallBounding = $wall.getBoundingClientRect();
     const wallBounding = this.wallBounding;
 
     // configuration
-    this.slotSize = { 
-      w: wallBounding.width / 4, 
-      h: 200 };
+    this.slotSize = {
+      w: wallBounding.width / 4,
+      h: 200
+    };
 
-    this.gridSize = { cols: 7, rows: false, gap: 5, offsetX: -.5, offsetY: -.5 };
+    this.gridSize = {
+      cols: 7,
+      rows: false,
+      gap: 5,
+      snap: false,
+      offsetX: -0.5,
+      offsetY: -0.5
+    };
 
-    this.contentRootPos = { 
+    this.contentRootPos = {
       x: -this.slotSize.w * this.gridSize.offsetX,
-      y: -this.slotSize.h * this.gridSize.offsetY,
+      y: -this.slotSize.h * this.gridSize.offsetY
     };
     const contentRootPos = this.contentRootPos;
-    
-    this.slotRootPos = {...contentRootPos};
+
+    this.slotRootPos = { ...contentRootPos };
     const slotRootPos = this.slotRootPos;
 
     // slot configs
@@ -42,14 +52,14 @@ export default class {
     let deltaX = 0;
     let deltaY = 0;
 
-    hammer.on('panstart', (e) => {
-      console.log('panstart');
+    hammer.on("panstart", e => {
+      console.log("panstart");
       const { x, y } = e.center;
       deltaY = y - contentRootPos.y;
       deltaX = x - contentRootPos.x;
     });
 
-    hammer.on('panmove', (e) => {
+    hammer.on("panmove", e => {
       const { x, y } = e.center;
       const { slotTotalWidth, slotTotalHeight } = this.slotConfig;
       const { contentTotalWidth, contentTotalHeight } = this.contentConfig;
@@ -76,19 +86,52 @@ export default class {
 
       console.log(contentRootPos);
 
-      this.$slots.forEach(($slot, i) =>{
+      this.$slots.forEach(($slot, i) => {
         this.updateSlotPosition($slot, i);
       });
     });
 
-    hammer.on('panend', (e) => {
-      console.log('panend');
-      deltaX = 0;
-      deltaY = 0;
-    })
+    hammer.on("panend", e => {
+      console.log("panend");
+      const { x, y } = e.center;
+      const { slotTotalWidth, slotTotalHeight } = this.slotConfig;
+      const { contentTotalWidth, contentTotalHeight } = this.contentConfig;
+
+      const { snap, gap, offsetX, offsetY } = this.gridSize;
+      const { w: slotW, h: slotH } = this.slotSize;
+
+      // total distance from original position
+      let distanceY = y - deltaY;
+      let distanceX = x - deltaX;
+
+      if (snap) {
+        distanceY = (slotH + gap) * Math.round(distanceY / (slotH + gap)) - (offsetY * slotH);
+        distanceX = (slotW + gap) * Math.round(distanceX / (slotW + gap)) - (offsetX * slotW);
+      }
+
+      // TODOS: take this out into a function
+      // loop content
+      contentRootPos.y = distanceY % contentTotalHeight;
+      contentRootPos.x = distanceX % contentTotalWidth;
+
+      // keep track of loop count so we can place correct data on slot
+      // since we're counting from the bottom right position (hence Math.ceil)
+      // if loop count > 0, remove 1 loop to make it seamless
+      this.slotLoop.y = -Math.ceil(distanceY / slotTotalHeight);
+      if (this.slotLoop.y < 0) this.slotLoop.y++;
+      this.slotLoop.x = -Math.ceil(distanceX / slotTotalWidth);
+      if (this.slotLoop.x < 0) this.slotLoop.x++;
+
+      // loop slot
+      slotRootPos.y = distanceY % slotTotalHeight;
+      slotRootPos.x = distanceX % slotTotalWidth;
+
+      this.$slots.forEach(($slot, i) => {
+        this.updateSlotPosition($slot, i);
+      });
+    });
 
     this.createSlots();
-
 
     this.getSlotGridPos = this.getSlotGridPos.bind(this);
     this.updateSlotPosition = this.updateSlotPosition.bind(this);
@@ -99,53 +142,79 @@ export default class {
     this.renderDataToSlot = this.renderDataToSlot.bind(this);
   }
 
-  getSlotGridPos (i = 0) {
-    const { slotCols:cols } = this.slotConfig;
+  getSlotGridPos(i = 0) {
+    const { slotCols: cols } = this.slotConfig;
     const col = i % cols;
     const row = ~~(i / cols);
     return { row, col };
   }
 
-  updateSlotPosition ($slot, i) {
+  getSlotPosFromGridPos(col, row) {
     const { slotCols, slotRows } = this.slotConfig;
-    const { contentCols, contentRows } = this.contentConfig;
-    const { x:rootX, y:rootY } = this.slotRootPos;
-    const { w:slotW, h:slotH } = this.slotSize;
-    const { x:loopX, y:loopY } = this.slotLoop;
+    const { x: rootX, y: rootY } = this.slotRootPos;
+    const { w: slotW, h: slotH } = this.slotSize;
     const { gap } = this.gridSize;
 
-    let contentCol, contentRow;
-    let col = parseInt($slot.getAttribute('data-slot-col'), 10);
-    let row = parseInt($slot.getAttribute('data-slot-row'), 10);
     // amount of slot that'll fit into the distance between rootX and this slot
     // basically, if slot's not in view -> move it to the other side of the grid
-    const slotHorizontalDistance = (Math.ceil(rootX / (slotW + gap)) + col);
+    const slotHorizontalDistance = Math.ceil(rootX / (slotW + gap)) + col;
     if (slotHorizontalDistance >= slotCols) col = col - slotCols;
-    if (slotHorizontalDistance < 0) col =  col + slotCols;
+    if (slotHorizontalDistance < 0) col = col + slotCols;
 
-    const slotVerticalDistance = (Math.ceil(rootY / (slotH + gap)) + row);
+    const slotVerticalDistance = Math.ceil(rootY / (slotH + gap)) + row;
     if (slotVerticalDistance >= slotRows) row = row - slotRows;
     if (slotVerticalDistance < 0) row = row + slotRows;
 
-    const slotX = rootX + (col * (slotW + gap));
-    const slotY = rootY + (row * (slotH + gap)); 
+    const slotX = rootX + col * (slotW + gap);
+    const slotY = rootY + row * (slotH + gap);
 
+    return {
+      slotX,
+      slotY,
+    }
+  }
+
+  updateSlotPosition($slot, i) {
+    const { slotCols, slotRows } = this.slotConfig;
+    const { contentCols, contentRows } = this.contentConfig;
+    const { x: loopX, y: loopY } = this.slotLoop;
+
+    let col = parseInt($slot.getAttribute("data-slot-col"), 10);
+    let row = parseInt($slot.getAttribute("data-slot-row"), 10);
+
+    let contentCol, contentRow;
+
+    const { slotX, slotY } = this.getSlotPosFromGridPos(col, row);
     $slot.style = `transform: translate(${slotX}px, ${slotY}px);`;
 
     // Content
-    contentCol = (col + (loopX * slotCols)) % contentCols;
-    contentRow = (row + (loopY * slotRows)) % contentRows;
+    contentCol = (col + loopX * slotCols) % contentCols;
+    contentRow = (row + loopY * slotRows) % contentRows;
 
-    $slot.setAttribute('data-content-row', contentCol);
-    $slot.setAttribute('data-content-col', contentRow);
-    
+    $slot.setAttribute("data-content-row", contentCol);
+    $slot.setAttribute("data-content-col", contentRow);
+
     const contentId = contentRow * contentCols + contentCol;
     $slot.textContent = this.renderDataToSlot(contentId);
   }
 
-  getSlotGridConfig () {
+  snap($slot, setting) {
+    let start = false;
+    const { fromPos, toPos, duration } = setting;
+
+    function step(timestamp) {
+      if (!start) start = timestamp;
+      const delta = timestamp - start;
+      const x = fromPos.x + (toPos.x - fromPos.x) / duration * delta;
+      const y = fromPos.y + (toPos.y - fromPos.y) / duration * delta;
+      $slot.style = `transform: translate(${x}px, ${y}px);`;
+      if (delta < duration) window.requestAnimationFrame(step);
+    }
+  }
+
+  getSlotGridConfig() {
     const { width, height } = this.wallBounding;
-    const { w:slotW, h:slotH } = this.slotSize;
+    const { w: slotW, h: slotH } = this.slotSize;
     const { gap } = this.gridSize;
 
     const slotCols = Math.round(width / (slotW + gap)) + 1;
@@ -154,19 +223,19 @@ export default class {
 
     const slotTotalWidth = (slotW + gap) * slotCols;
     const slotTotalHeight = (slotH + gap) * slotRows;
-    
+
     return {
       slotCols,
       slotRows,
       slotAmount,
       slotTotalWidth,
-      slotTotalHeight,
-    }
+      slotTotalHeight
+    };
   }
 
-  getContentGridConfig () {
-    const { gap, cols:contentCols } = this.gridSize;
-    const { w:slotW, h:slotH } = this.slotSize;
+  getContentGridConfig() {
+    const { gap, cols: contentCols } = this.gridSize;
+    const { w: slotW, h: slotH } = this.slotSize;
     const contentRows = Math.ceil(this.data.length / contentCols);
 
     const contentTotalWidth = (slotW + gap) * contentCols;
@@ -176,19 +245,20 @@ export default class {
       contentCols,
       contentRows,
       contentTotalWidth,
-      contentTotalHeight,
-    }
+      contentTotalHeight
+    };
   }
 
-  createSlots () {
+  createSlots() {
     const { slotAmount } = this.slotConfig;
     this.$slots = [];
 
     const { w, h } = this.slotSize;
     const head = document.head;
-    const css = document.createElement('style');
-    css.type = 'text/css';
-    css.appendChild(document.createTextNode(`
+    const css = document.createElement("style");
+    css.type = "text/css";
+    css.appendChild(
+      document.createTextNode(`
     #${this.$wall.id} .slot {
       position: absolute;
       display: block;
@@ -196,18 +266,19 @@ export default class {
       top: 0; left: 0;
       width: ${w}px; height: ${h}px;
     }
-    `));
+    `)
+    );
     head.appendChild(css);
 
     for (let i = 0; i < slotAmount; i++) {
-      const $slot = document.createElement('div');
+      const $slot = document.createElement("div");
       const { row, col } = this.getSlotGridPos(i);
 
       $slot.classList.add(`slot`, `slot-${i}`);
 
-      $slot.setAttribute('data-slot-id', i);
-      $slot.setAttribute('data-slot-row', row);
-      $slot.setAttribute('data-slot-col', col);
+      $slot.setAttribute("data-slot-id", i);
+      $slot.setAttribute("data-slot-row", row);
+      $slot.setAttribute("data-slot-col", col);
 
       this.updateSlotPosition($slot, i);
       this.$wall.appendChild($slot);
@@ -216,7 +287,7 @@ export default class {
     }
   }
 
-  renderDataToSlot (i) {
+  renderDataToSlot(i) {
     // this is not accounting for the empty slot,
     // which is why the top of the scroll doesn't loop.
     // But it makes the wall more seamless
